0abdbbff08179434af6bb4d18c3530f6a8ef893f
muni-adifice <muni@adificetechnologies.com>
2026-01-21 09:10:40 +0530

    Profile partially implemented

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,24 +1,36 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
 
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import Dashboard from "../pages/auth/Dashboard";
+import { Profile } from "../components/Profile";
 
-const PrivateRoute = ({ children }) => {
-  const { isAuth, loading } = useAuth();
-  if (loading) return <div>Loading...</div>;
-  return isAuth ? children : <Navigate to="/login" />;
-};
 
-export default function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-         <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+// export default function AppRoutes() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+//          <Route path="/dashboard" element={<Dashboard />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+
+function AppRoutes(){
+    return(
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+             <Route path="/dashboard" element={<Dashboard />} />  
+             <Route path="/profile" element={<Profile />} />
+             
+          </Routes>
+        </BrowserRouter>
+    )
 }
+ 
+export default AppRoutes;
